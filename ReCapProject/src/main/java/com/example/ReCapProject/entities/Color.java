6cc6fed55a851c3c7247dd25e1ebbdb5d793bd55
zeynepkesim <78529576+zeynepkesim@users.noreply.github.com>
2021-09-16 11:34:23 +0300

    reCapProject
    1. isterler

---
+++ ReCapProject/src/main/java/com/example/ReCapProject/entities/Color.java
@@ -0,0 +1,32 @@
+package com.example.ReCapProject.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "colors")
+public class Color {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "color_id")
+	private int colorId;
+	
+	@Column(name = "color_name")
+	private String colorName;
+	
+	@OneToOne
+	private Car car;
+}
